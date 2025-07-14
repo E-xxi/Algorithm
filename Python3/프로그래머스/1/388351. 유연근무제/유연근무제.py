27a9129f2d796a3ed86fd57f0abb114c41131626
E-xxi <70554917+E-xxi@users.noreply.github.com>
2025-07-14 16:19:36 +0900

    [level 1] Title: 유연근무제, Time: 13.43 ms, Memory: 9.84 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -0,0 +1,31 @@
+def cal_standard_time(standard):
+    # 시간범위 재설정
+    # 958 + 10 -> 1008이어야됨. 
+    hour = standard // 100
+    minute = standard % 100 +10
+    if minute >= 60:
+        hour += 1
+        minute -= 60
+    return hour*100 + minute
+
+
+def solution(schedules, timelogs, startday):
+    answer = 0
+    
+    for standard, person in zip(schedules, timelogs):
+        late = False
+        for i, checked in zip(range(0,7), person):
+            day = ( startday + i ) % 7
+            print(day, standard, i, checked)
+            
+            if (day == 6) or (day == 0): # 주말
+                continue
+            
+            if cal_standard_time(standard)< checked: #지각하면
+                late = True
+                break
+            
+        if late == False:
+            answer+=1
+            
+    return answer
