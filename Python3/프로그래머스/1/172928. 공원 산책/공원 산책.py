dec54d45bc4c8113d29e8d6ad61963549c020af4
E-xxi <70554917+E-xxi@users.noreply.github.com>
2025-08-14 11:58:39 -0500

    [level 1] Title: 공원 산책, Time: 0.46 ms, Memory: 9.44 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -0,0 +1,40 @@
+def solution(park, routes): #y, x 순으로 출력할것.    
+    # 0 0 0
+    # 0 0 0<- (1, 2)
+    x, y = 0,0 # 시작지점
+    H, W = len(park), len(park[0])
+    obstacles = []
+    for u in range(len(park)):
+        if 'S' in park[u]:
+            y = park[u].index('S')
+            x = u
+        if 'X' in park[u]:
+            for v in range(len(park[u])):
+                if 'X' == park[u][v]:
+                    obstacles.append([u,v])
+                
+    #print('시작지점: ', x,y)
+    #print('obstacles: ', obstacles)
+    
+    way = {'E': (0,1), 'S':(1,0), 'W':(0,-1), 'N':(-1,0)}
+    for r in routes:
+        d, a = r.split(' ')[0], int(r.split(' ')[1])
+        ndx, ndy = way[d][0]*a, way[d][1]*a
+        #범위 초과 고려
+        if (x+ndx < 0) or (x+ndx >= H) or (y+ndy >= W) or (y+ndy < 0):
+            continue
+        
+        #장애물 확인
+        minx, maxx = min(x, x+ndx), max(x, x+ndx)
+        miny, maxy = min(y, y+ndy), max(y, y+ndy)
+        for obs in obstacles:
+            #print(minx, obs[0], maxx, miny, obs[1], maxy)
+            if (minx <= obs[0] <= maxx) and (miny <= obs[1] <= maxy): #중간에 장애물이 있다면
+                #print(f'방향 {d}, 거리 {ndx, ndy}, 현재 위치 {x, y} 유지') 
+                break
+        else:
+            #print(f'방향 {d}, 거리 {ndx, ndy}, 현재 위치 {x, y} 다음 위치 {x+ndx, y+ndy}')   
+            x += ndx
+            y += ndy
+    
+    return [x,y]
