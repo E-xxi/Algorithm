bb4e31d68593b65d495b7934d0b738ea64311d45
E-xxi <70554917+E-xxi@users.noreply.github.com>
2025-08-14 14:35:30 -0500

    [level 1] Title: 대충 만든 자판, Time: 39.84 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,19 @@
+def solution(keymap, targets):
+    answer = []
+    
+    for words in targets:
+        cnt = 0
+        for c in words:
+            temp = 1000
+            for key in keymap:
+                if c in key:
+                    temp = min(temp, key.index(c)+1)
+            print(c, temp)
+            if temp < 1000:
+                cnt += temp
+            else:
+                cnt = -1
+                break
+        answer.append(cnt)
+            
+    return answer
