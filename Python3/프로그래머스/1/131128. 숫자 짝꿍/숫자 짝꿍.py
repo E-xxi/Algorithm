44482a96e2df6f2f54b0aea4557238a9503731c8
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-01-13 17:41:32 +0900

    [level 1] Title: 숫자 짝꿍, Time: 367.10 ms, Memory: 69.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+def commonalpha(X,Y):    # 공통 알파벳만 추출
+    common = []
+    l = Counter(list(set(X)) + list(set(Y))).items()
+
+    for k, v in l:
+        if v == 2:
+            common.append(k)
+        
+    return sorted(common)
+
+def solution(X, Y):
+    both = commonalpha(X,Y)
+    answer = []
+    
+    #print(f'both {both}')
+    if not both:
+        return '-1'
+    
+    if (len(both) == 1) and (both[0] == '0'):
+        return '0'
+    
+    #각각 양쪽에 몇개있는지 알아야함
+    for b in both:
+        count = min(X.count(b), Y.count(b))
+        if count:
+            answer = answer + [b] * count
+            #print(a, count, new)
+    
+    return ''.join(sorted(answer,reverse = True))
