814e6176330f5f45d7b50bc619abb1e5f347f690
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-01-21 17:25:53 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 9.15 MB -BaekjoonHub

--- Python3/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ Python3/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,15 +1,15 @@
 def solution(n, words):
-    person = 1
-    cnt = 1
-    l = []
-    last = words[0][0]
-    for i in range(len(words)):
-        person = i%n+1
-        cnt = i//n+1
-        if (words[i] in l) or (last != words[i][0]):
-            return [person,cnt]
-        l.append(words[i])
-        last = words[i][-1]
-    print('return')
-    return [0,0]
-
+    word_list = []
+    
+    for idx, word in enumerate(words):
+        if word_list: #두번째 차례부터
+            if word[0] != word_list[-1][-1]: #잘못되 단어를 말했거나
+                break
+            elif len(set(word_list)) == len(set(word_list + [word])): #이전에 등장했던 단어거나     
+                break
+        word_list.append(word)
+        
+    else: #탈락자 없으면 
+        return [0, 0]
+    
+    return [idx%n +1, idx//n +1]
