c818c4bec9e3f5ef3de1002cd9b00e9164ab769a
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-01-26 12:15:30 +0900

    [level 2] Title: 미로 탈출, Time: 31.21 ms, Memory: 9.41 MB -BaekjoonHub

--- Python3/프로그래머스/2/159993. 미로 탈출/미로 탈출.py
+++ Python3/프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -1,37 +1,45 @@
-def check_depth(target, lever, maps):
-    N,M = len(maps), len(maps[0])   
-    depth = 1
-    tree_list = [lever]
+from collections import deque
+def find_index(target, maps):
+    for idx, row in enumerate(maps):
+        if target in row:
+            return (idx,row.index(target))
+
+def is_valid(x,y, maps):
+    N,M = len(maps), len(maps[0])
+    return (0<=x<N) and (0<=y<M) and (maps[x][y] != 'X')
+
+def find_target(L, target, maps):
+    #최단거리니까 bfs
+    
+    q = deque([[L[0],L[1],1]])
     dxy = [(0,1),(0,-1),(1,0),(-1,0)]
     
-    while tree_list:          
-        temp = []
-        for x,y in tree_list: #레버에서 내려가면서 확인
-            maps[x][y] = 'X'
-            for dx, dy in dxy: 
-                if (0<=x+dx<N) and (0<=y+dy<M) and ((x+dx,y+dy) not in temp) :
-                    if maps[x+dx][y+dy] == target:
-                        return depth
-                    elif maps[x+dx][y+dy] != 'X':
-                        temp.append((x+dx,y+dy))
-        tree_list = temp
-        #print(tree_list)
-        depth += 1 
-    return 0
+    while q:
+        cx, cy, cnt = q.popleft()
+        #print(cx,cy,cnt)
+        
+        for dx,dy in dxy:
+            nx, ny = cx+dx, cy+dy
+            if is_valid(nx,ny, maps): #지나갈수 있으면
+                if maps[nx][ny] == target:
+                    return cnt
+                q.append((nx,ny,cnt+1))
+                maps[nx][ny] = 'X'
+    else:
+        return 0
+    
+    
+    return cnt
 
 def solution(maps):
     answer = 0
-    #레버를 기준으로 트리를 만들어서 
-    #각각 start까지 뎁스 exit까지 뎁스를 구하면 될듯?
-    maps = [list(row) for row in maps]
+    maps = list(map(list, maps)) #리스트로 변환
     
-    for i in range(len(maps)):
-        if 'L' in maps[i]:
-            lever = i, maps[i].index('L')
+    lever = find_index('L',maps)
     
-    #maps.copy() 얕은복사때문에 충돌
-    ls = check_depth('S', lever, [row[:] for row in maps])
-    le = check_depth('E', lever,  [row[:] for row in maps]) 
-    #print(ls,le)
+    #L-> S
+    start = find_target(lever, 'S',[m[:] for m in maps])
+    end = find_target(lever, 'E',[m[:] for m in maps])
     
-    return ls+le if ls and le else -1
+
+    return start+end if start and end else -1
