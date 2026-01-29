1272c9b7130e56b83bfd7ed6578362ca93dd575c
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-01-29 16:34:23 +0900

    [level 2] Title: 튜플, Time: 26.25 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=python3#) 
 
 ### 성능 요약
 
-메모리: 15.7 MB, 시간: 226.72 ms
+메모리: 19.8 MB, 시간: 26.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 29일 16:18:50
+2026년 01월 29일 16:34:21
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,33 +1,6 @@
-import functools
-
-def compared(x, answer):
-    return -1 if x not in answer else 1
-
+from collections import Counter
 def solution(s):
-    #중복있음, 순서 인정
-    answer = []
     #s가 표현하고 있는 튜플
-    comb_nums = []
-    for ch in s.split('}'):
-        if ch != '':
-            comb_nums.append(list(map(int, ch.split('{')[-1].split(','))))
+    comb_nums = dict(Counter(s.replace('{','').replace('}','').split(",")))
     
-    #일단 기본적으로 순서를 가지거 있었는데, 괄호 안의 숫자 숫서는 바뀜
-    #전체를 구한 다음에 줄여나가면서 없는 숫자 가 마지막인듯
-    #{3,2,4,1}부터 시작해서 num에 없는 숫자를 맨 뒤로?
-    comb_nums.sort(key = lambda x:-len(x))
-    answer = comb_nums.pop()
-    for num in comb_nums[::-1]:
-        for a in answer:
-            try:
-                num.pop(num.index(a))
-            except:
-                answer.append(a)
-                break
-        answer+=num
-    # d
-    
-    
-    
-    
-    return answer
+    return list(map(int, dict(sorted(comb_nums.items(), key = lambda x:-x[1])).keys()))
