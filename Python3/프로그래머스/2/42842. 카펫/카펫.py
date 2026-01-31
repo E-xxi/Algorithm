ac4bce793ba9c88ce63be5c8689c14f56acf3f23
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-01-31 19:57:24 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 9.17 MB -BaekjoonHub

--- Python3/프로그래머스/2/42842. 카펫/카펫.py
+++ Python3/프로그래머스/2/42842. 카펫/카펫.py
@@ -1,9 +1,14 @@
 def solution(brown, yellow):
-    #print(bro)
-    for i in range(1, brown//2+1): #가로
-        for j in range(1, brown//2+1):
-            if (yellow == (i-2)*(j-2)) and (brown == (i*j - (i-2)*(j-2))):
-                return [max(i,j), min(i,j)]
+    answer = []
+    total = brown+yellow
     
+    #테두리 한줄만 갈색이면
+    #노란색이 : i-2,j-2이면 갈색은: i, j
     
+    for i in range(3, int(total**(1/2))+1):
+        if (total % i == 0):
+            j = total // i
+            if ((i-2) * (j-2)) == yellow:
+                answer = [i,j]
     
+    return sorted(answer,reverse = True)
