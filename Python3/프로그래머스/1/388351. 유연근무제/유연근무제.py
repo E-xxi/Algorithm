2444d4d9ffea21f88939844d3e0cad875555a531
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-03-05 23:51:13 +0900

    [level 1] Title: 유연근무제, Time: 1.61 ms, Memory: 9.5 MB -BaekjoonHub

--- Python3/프로그래머스/1/388351. 유연근무제/유연근무제.py
+++ Python3/프로그래머스/1/388351. 유연근무제/유연근무제.py
@@ -1,31 +1,21 @@
-def cal_standard_time(standard):
-    # 시간범위 재설정
-    # 958 + 10 -> 1008이어야됨. 
-    hour = standard // 100
-    minute = standard % 100
-    if minute >= 50:
-        hour += 1
-        minute -= 60
-    return hour*100 + minute +10
-
-
 def solution(schedules, timelogs, startday):
     answer = 0
+    #출근희망시각+10분까지 출근
+    #startday %7 +1, 6,7은 주말
     
-    for standard, person in zip(schedules, timelogs):
-        late = False
-        for i, checked in zip(range(0,7), person):
-            day = ( startday + i ) % 7
-            print(day, standard, i, checked)
+    for s, timelog in zip(schedules, timelogs):
+        hour,minutes = s//100, s%100
+        hour += (minutes+10) //60
+        minutes = (minutes+10) % 60
+        
+        for i in range(7):
+            day = ((startday+i-1)%7+1)
             
-            if (day == 6) or (day == 0): # 주말
-                continue
-            
-            if cal_standard_time(standard)< checked: #지각하면
-                late = True
+            if day not in [6,7] and (hour*100+minutes < timelog[i]):
                 break
+        else:
+            answer +=1
             
-        if late == False:
-            answer+=1
+                
             
     return answer
