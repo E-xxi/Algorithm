4f2d9941c731a093d8b9e81a30cbf8bfc3a29424
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-03-07 14:49:38 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

--- Python3/프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ Python3/프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
 
 ### 성능 요약
 
-메모리: 9.16 MB, 시간: 0.02 ms
+메모리: 9.13 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 13일 14:07:30
+2026년 03월 07일 14:49:34
 
 ### 문제 설명
 
--- Python3/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
+++ Python3/프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.py
@@ -1,21 +1,17 @@
-def solution(players, m, k): #m 증설시점, k 운영시간
+def solution(players, m, k):
     answer = 0
-
-    server_cnt = 0 #현재 서버 개수
-    server_time = {}
+    server = [0]*24 #시점,개수
     
-    for i in range(len(players)):
-        #타임아웃된 서버 삭제
-        if i-k in server_time.keys():
-            server_cnt -= server_time[i-k]
+    for i in range(24):
+        if i-k >= 0 :
+            server[i-k] = 0
         
-        # 현재 필요한 서버 개수 
-        if server_cnt < players[i]//m:
-            cnt = players[i] // m - server_cnt
-            answer += cnt
-            server_time[i] = cnt #증설된 서버의 시간을 저장
-            server_cnt = players[i] // m
+        #필요한 서버만큼 추가 
+        if players[i] // m > sum(server):
+            add_server = players[i] // m - sum(server)
+            server[i] = add_server
+            answer += add_server
+            #print(i,': ', players[i], sum(server), add_server)
         
-        #print(i, f'~ {i+1}시간', server_cnt, answer)   
         
     return answer
