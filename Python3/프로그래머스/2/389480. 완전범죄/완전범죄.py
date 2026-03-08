232d4815deb2bb609a79376c73c1306d45f00fa8
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-03-08 19:39:36 +0900

    [level 2] Title: 완전범죄, Time: 47.87 ms, Memory: 13.2 MB -BaekjoonHub

--- Python3/프로그래머스/2/389480. 완전범죄/완전범죄.py
+++ Python3/프로그래머스/2/389480. 완전범죄/완전범죄.py
@@ -1,3 +1,37 @@
+from collections import deque
+def solution(info, n, m):
+    answer = float('inf')
+    
+    l = len(info)
+    que = deque([])
+    if info[0][0] < n:
+        que.append([0,info[0][0],0])
+    if info[0][1] < m:
+        que.append([0,0,info[0][1]])
+    
+    visited = set()
+    
+    while que:        
+        idx, cn, cm = que.popleft()
+        
+        if (idx, cn, cm) in visited:
+            continue
+        visited.add((idx, cn, cm))
+        
+        if idx+1 >= l:
+            answer = min(cn,answer)
+            #print(idx,cn,cm)
+            continue
+        
+        newa, newb = info[idx+1]
+        if (cn+newa < n) and (cm < m):
+            que.append([idx+1, cn+newa, cm])
+        if (cn < n) and (cm+newb < m):
+            que.append([idx+1, cn, cm+newb])
+    
+    return -1 if answer == float('inf') else answer
+        
+'''
 def solution(info, n, m):
     answer = 0
     
@@ -22,4 +56,4 @@ def solution(info, n, m):
         dp = temp
     
     print(min(dp))
-    return -1 if min(dp) == float('inf') else min(dp)
+    return -1 if min(dp) == float('inf') else min(dp)'''
