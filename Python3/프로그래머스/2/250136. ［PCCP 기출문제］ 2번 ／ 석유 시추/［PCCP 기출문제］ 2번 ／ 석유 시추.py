d8c096d1d1aa25d3ef6ede1432e6d985245fe4cd
E-xxi <70554917+E-xxi@users.noreply.github.com>
2026-06-26 18:25:57 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 213.11 ms, Memory: 22.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
+++ Python3/프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.py
@@ -16,17 +16,15 @@ def solution(land):
         
         return False
     
-    #petrol_range: num각 석유가 위치한 컬럼의 범위
-    #petrol_size: num각 얼마나 가지고 있는지
-    column_num = defaultdict(set)
-    petrol_size = defaultdict(lambda:1) #초기값 1로 설정
     
     def dfs(num,a,b):
         s = [(a, b)]
         land[a][b] = -1
         
         dxy =[(0,-1), (-1,0), (0,1) , (1,0)]
+        oil = 1
         
+        column_range = set([b])
         while s: #연결되어있으면 같은 숫자로 칠하게 
             x, y  = s.pop()
             
@@ -35,26 +33,21 @@ def solution(land):
                 
                 if is_valid(nx,ny): #범위 안이고, 방문하지 않은 land
                     if land[nx][ny] == 1: #석유 존재
-                        column_num[ny].add(num)
-                        petrol_size[num] += 1  
+                        column_range.add(ny)
+                        oil += 1  
                         
                     s.append((nx,ny))
                     land[nx][ny] = -1
-                    
+        
+        for c in list(column_range):
+            answer[c] += oil
 
     num = 1
     for i in range(N):
         for j in range(M):
             if land[i][j] == 1:
-                column_num[j].add(num)  #첫방문지도 포함해야됨
                 dfs(num, i, j)
             num+=1
-        
-    #print(f'size: {dict(sorted(petrol_size.items()))}')
-    #print(f'range: {dict(sorted(column_num.items()))}')
-    
-    for c,v in column_num.items():
-        for idx in list(v):
-            answer[c] += petrol_size[idx]
-    
+
+    #print(answer)
     return max(answer)
